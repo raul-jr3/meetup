bddeaa3ca8b4210626ce52c8e0236ce7ec29e99b
Rahul Prasanna <rahul.srivatsav1995@gmail.com>
2018-01-07 13:23:54 +0530

    update meetup model

--- core/models.py
+++ core/models.py
@@ -3,7 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Meetup(models.Model):
-    name = models.CharField(max_length = 200)
+    meetup_name = models.CharField(max_length = 200)
     created = models.DateTimeField(auto_now_add = True)
     created_by = models.ForeignKey(User, related_name = 'created_meetups', on_delete = models.CASCADE)
     meetup_image = models.ImageField(upload_to = "%Y/%m/%d/meetup_cover")
+    attendees = models.ForeignKey(User, related_name = "attending", on_delete = models.CASCADE)
+    not_sure = models.ForeignKey(User, related_name = "not_sure", on_delete = models.CASCADE)
+    description = models.TextField()
+    meetup_time = models.DateTimeField()
+    meetup_place = models.CharField(max_length = 300)
+
+    def get_absolute_url(self):
+        return reverse('core:meetup_detail', args = [self.id])
