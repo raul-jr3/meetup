02715174ddc692119442d7a9df4e317a2af73937
Rahul Prasanna <rahul.srivatsav1995@gmail.com>
2018-01-07 13:36:14 +0530

    made attendees and not_sure as optional

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Meetup
+
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ['meetup_name', 'created_by', 'meetup_time', 'meetup_place',
+                    'meetup_image', 'description']
+    list_filter = ['created_by', 'meetup_time']
+
+admin.site.register(Meetup, MeetupAdmin)
